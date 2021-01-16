9524ee45e09d0d56a441ed4e2ad8af1cb98734b8
Banupriyaramesh <76839486+Banupriyaramesh@users.noreply.github.com>
2021-01-16 18:45:54 +0530

    oops concept

---
+++ cre.py
@@ -0,0 +1,11 @@
+class employee:
+	def __init__(self,name,age,salary)
+	self.name=name
+	self.age=age
+	self.salary=salary
+	def displayemployee(self):
+		print("Name is:",self.name,"age is:",self.age,"Your salary is:",self.salary)
+emp1=employee("Raja",20,20000)
+emp2=employee("priya",25,30000)
+emp1.displayemployee()
+emp2.displayemployee()
